f1f4c5adf6edde71170373adb8594e913eeca66f
jajimentol <aniljturan@gmail.com>
2025-08-18 13:40:34 +0300

    onboardingKit started

--- Sources/OnboardingKit/OnboardingKit.swift
+++ Sources/OnboardingKit/OnboardingKit.swift
@@ -1,2 +1,22 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import UIKit
+
+@MainActor
+public class OnboardingKit {
+    
+    public init() {}
+    
+    private var onboardingViewController: OnboardingViewController = {
+        let controller = OnboardingViewController()
+        controller.modalTransitionStyle = .crossDissolve
+        controller.modalPresentationStyle = .fullScreen
+        return controller
+    }()
+    
+    public func launchOnboarding(root: UIViewController) {
+        root.present(onboardingViewController, animated: true, completion: nil)
+    }
+    
+    public func dismissOnboarding() {
+        
+    }
+}
