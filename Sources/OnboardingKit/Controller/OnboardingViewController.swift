9656d1ceb2ff38bcdf07f809322c0b7455be8645
jajimentol <aniljturan@gmail.com>
2025-08-18 13:40:41 +0300

    onboardingVC created

---
+++ Sources/OnboardingKit/Controller/OnboardingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingViewController.swift
+//  OnboardingKit
+//
+//  Created by Anil Turan on 18.08.2025.
+//
+
+import UIKit
+
+open class OnboardingViewController: UIViewController {
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
