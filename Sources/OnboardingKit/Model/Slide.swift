167972ec9cd33eee6329abae1d98191e0023f281
jajimentol <aniljturan@gmail.com>
2025-08-18 13:43:27 +0300

    slide model created

---
+++ Sources/OnboardingKit/Model/Slide.swift
@@ -0,0 +1,18 @@
+//
+//  Slide.swift
+//  OnboardingKit
+//
+//  Created by Anil Turan on 18.08.2025.
+//
+
+import UIKit
+
+public struct Slide {
+    public let image: UIImage
+    public let title: String
+    
+    public init(image: UIImage, title: String) {
+        self.image = image
+        self.title = title
+    }
+}
