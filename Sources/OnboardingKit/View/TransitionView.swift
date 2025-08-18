2ff24c11a8b67fbe044a408015a6799cdbfdab65
jajimentol <aniljturan@gmail.com>
2025-08-18 16:19:42 +0300

    image transition added

--- Sources/OnboardingKit/View/TransitionView.swift
+++ Sources/OnboardingKit/View/TransitionView.swift
@@ -50,6 +50,7 @@ class TransitionView: UIView {
         
     
     private let slides: [Slide]
+    private var currentIndex: Int = -1
     
     init(content: OnboardingContent) {
         self.slides = content.slides
@@ -73,8 +74,31 @@ class TransitionView: UIView {
         timer?.schedule(deadline: .now(), repeating: 3, leeway: .seconds(1))
         timer?.setEventHandler { [weak self] in
             guard let self else { return }
-            print("show next")
+            DispatchQueue.main.async {
+                self.showNext()
+            }
         }
+    }
+    
+    private func showNext() {
+        
+        let image: UIImage
+        
+        if slides.indices.contains(currentIndex + 1) {
+            image = slides[currentIndex+1].image
+            currentIndex += 1
+        } else {
+            image = slides.first!.image
+            currentIndex = 0
+        }
+        
+        UIView.transition(
+            with: imageView,
+            duration: 0.5,
+            options: .transitionCrossDissolve) {
+                self.imageView.image = image
+            }
+        
     }
     
     private func setupView() {
