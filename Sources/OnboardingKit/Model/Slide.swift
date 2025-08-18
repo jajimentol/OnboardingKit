4c953ea64c2c5c8d68c3aac19597f5597af870a3
jajimentol <aniljturan@gmail.com>
2025-08-18 13:49:06 +0300

    added docstrings

--- Sources/OnboardingKit/Model/Slide.swift
+++ Sources/OnboardingKit/Model/Slide.swift
@@ -7,10 +7,15 @@
 
 import UIKit
 
+/// Represents a single slide in the onboarding process.
 public struct Slide {
+    /// The image to be displayed on the slide.
     public let image: UIImage
+    
+    /// The title of the slide.
     public let title: String
     
+    /// Initializes a new slide with the specified image and title.
     public init(image: UIImage, title: String) {
         self.image = image
         self.title = title
--- Sources/OnboardingKit/OnboardingKit.swift
+++ Sources/OnboardingKit/OnboardingKit.swift
@@ -1,9 +1,16 @@
 import UIKit
 
+
+/// The kit that manages the onboarding process in the application.
+/// This class is responsible for presenting the onboarding view controller with the provided slides and tint color.
 @MainActor
 public class OnboardingKit {
     
-    public init() {}
+    /// The slides to be displayed in the onboarding process.
+    private let slides: [Slide]
+    
+    /// The tint color to be used in the top bars.
+    private let tintColor: UIColor
     
     private var onboardingViewController: OnboardingViewController = {
         let controller = OnboardingViewController()
@@ -12,10 +19,18 @@ public class OnboardingKit {
         return controller
     }()
     
+    /// Initializes the OnboardingKit with the provided slides and tint color.
+    public init(slides: [Slide], tintColor: UIColor) {
+        self.slides = slides
+        self.tintColor = tintColor
+    }
+    
+    /// Launches the onboarding process by presenting the onboarding view controller.
     public func launchOnboarding(root: UIViewController) {
         root.present(onboardingViewController, animated: true, completion: nil)
     }
     
+    /// Dismisses the onboarding view controller.
     public func dismissOnboarding() {
         
     }
